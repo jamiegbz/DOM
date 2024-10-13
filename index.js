b1963f02b61368d3f7d884ba51d624d72c80bb9d
Jamie  Gomez <jamiegomez@MacBook-Air.local>
2024-10-12 23:31:05 -0400

    updated and finished

--- index.js
+++ index.js
@@ -13,14 +13,14 @@
  */
 
 const button= document.getElementById('button')
-
+//number starting at 0 on table
 let number = 0
 
 button.addEventListener('click', (event) =>{
     //event.preventDefault will not make the page refreash
     event.preventDefault()
 
-    //var to hold the values 
+    // 3 variables to hold the values 
     let disneyCharacter = document.getElementById('disneyCharacter').value
     let show = document.getElementById('show').value
     let firstName = document.getElementById('firstName').value
@@ -28,18 +28,23 @@ button.addEventListener('click', (event) =>{
     //create tr node append to form
     let newTableRow = document.createElement('tr')
 
+    // the table data to hold hold all four 
+    //1
     let numberNode = document.createElement('td')
     numberNode.innerHTML = number
     newTableRow.append(numberNode)
 
+    //2
     let disneyCharacterNode = document.createElement('td')
     disneyCharacterNode.innerHTML = disneyCharacter
     newTableRow.append(disneyCharacterNode)
 
+    //3
     let showNode = document.createElement('td')
     showNode.innerHTML = show
     newTableRow.append(showNode)
 
+    //4
     let firstNameNode = document.createElement('td')
     firstNameNode.innerHTML = firstName
     newTableRow.append(firstNameNode)
